0de150e705d3daa560a46a1e87bfad79df70d5da
Cong Wang <wangcong525@hotmail.com>
2024-08-29 21:57:58 -0400

    transaction and index updated

--- models/index.js
+++ models/index.js
@@ -1,13 +1,46 @@
 const User = require('./User');
-const Project = require('./Project');
+const Book = require('./Book');
+const Transaction = require('./Transaction');
 
-User.hasMany(Project, {
+
+User.hasMany(Book, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+Book.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+User.hasMany(Transaction, {
+  as: 'seller',
+  foreignKey: 'seller_id',
+  onDelete: 'CASCADE',
+});
+
+Transaction.belongsTo(User, {
+  as: 'seller',
+  foreignKey: 'seller_id',
+});
+
+User.hasMany(Transaction, {
+  as: 'buyer',
+  foreignKey: 'buyer_id',
+  onDelete: 'CASCADE',
+});
+
+Transaction.belongsTo(User, {
+  as: 'buyer',
+  foreignKey: 'buyer_id',
+});
+
+Book.hasMany(Transaction, {
+  foreignKey: 'book_id',
+});
+
+Transaction.belongsTo(Book, {
+  foreignKey: 'book_id',
+});
+
+
+module.exports = { User, Book, Transaction};
