41d419f209f2c6a870f4d7ebce2fd5c3930cbc94
Cong Wang <77494072+hydy1943cong@users.noreply.github.com>
2024-09-19 19:14:20 -0400

    Cong, Christine's info added

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
-
+const aboutUsRoutes=require('./aboutUsRoutes');
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+
+router.use('/about', aboutUsRoutes);
+
 
 
 module.exports = router;
