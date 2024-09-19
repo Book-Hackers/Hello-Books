94551b9cb9c7468fd678e3553c305ff107e050d2
Cong Wang <77494072+hydy1943cong@users.noreply.github.com>
2024-09-19 10:54:04 -0400

    About us page works

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const aboutUsRoutes=require('./aboutUsRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-
+router.use('/about', aboutUsRoutes);
 
 module.exports = router;
